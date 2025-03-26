cc40188dbe3f5b409b4ee46b87f626cc57f9067c
genti194 <gentdaka6@gmail.com>
2025-03-26 19:26:46 +0100

    medium

---
+++ module 28/challenges/medium.js
@@ -0,0 +1,7 @@
+btn1.onclick = function () {
+    txt1.style.color="green";
+    txt1.style.backgroundColor="orange";
+    txt1.style.textAlign="center";
+    txt1.style.fontSize="100px";
+    txt1.style.padding="24px";  
+  }
