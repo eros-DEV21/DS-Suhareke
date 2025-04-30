431b54b7bbd77b58a467e5d75022694eb35c098d
genti194 <gentdaka6@gmail.com>
2025-04-30 19:18:52 +0200

    loops

---
+++ module 32/main.js
@@ -0,0 +1,39 @@
+/*var input =document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text = document.getElementById('teksti_id');
+
+button.onclick=function(){
+    text.innerHTML=input.value;
+}*/
+
+/*var text="The best school in the world is Digjital School!";
+var result=text.search("Digjital School")
+document.getElementById("result1").innerHTML = result;*/
+
+/*var text="The best school in the world is Digjital School!";
+var result=text.search(/Digjital School/)
+document.getElementById("result2").innerHTML=result;*/
+
+/*var text="The best school in the world is Digjital School!";
+var result=text.replace(/Digjital School/,"Another School");
+document.getElementById("result3").innerHtml=result;*/
+
+/*var text="abcdef";
+var regex=new RegExp('abc');
+document.getElementById("result4").innerHTML=regex.test(text);*/
+
+/*var text= "My school is the best school in  the world";
+var regex=/school/g;
+document.getElementById("result5").innerHTML=text.match(regex);*/
+
+/*var text= "Digjital School is the best school in the world";
+var regex=/i/g;
+document.getElementById("result6").innerHTML=text.match(regex);*/
+
+/*var text="Djitial School is the best school in the world";
+var regex=/[abc]/g;
+document.getElementById(result7).innerHTML=text.match(regex);*/
+
+var text="Digjital School is in top 10 best schools in the world!";
+var regex=/[0-9]/g;
+document.getElementById("result8").innerHTML=text.match(regex);
